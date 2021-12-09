178bb1619b0342d0781cd93c041cfc947b5046f0
Antoine <antoine.zimmermann@epitech.eu>
2021-12-09 22:13:40 +0300

    toujours envie de mourrir

--- pages/adminUsers.tsx
+++ pages/adminUsers.tsx
@@ -1,8 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
-import { Form, Input, Button, Checkbox } from 'antd'
+import { Form, Input, Button, Checkbox, Card } from 'antd'
 import axios from 'axios'
+import Cards from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Modal from '@mui/material/Modal';
 
 //backend
 import { isUserLogged } from 'backend/utils/tokenChecker'
@@ -14,7 +19,6 @@ import { UserState } from 'common/types';
 import Layout from 'components/global/Layout/Layout'
 import Navbar from 'components/global/Navbar/Navbar'
 import Footer from 'components/global/Footer/Footer'
-import UserCard from 'components/pages/Admin/userCard'
 
 //css
 import * as Styled from 'styles/pages/signin'
@@ -40,6 +44,18 @@ const adminUsers = ({ user }: Props): JSX.Element => {
   const auth = useAuth()
   const [mounted, setMounted] = useState<boolean>(false)
   const [users, setUsers] = useState(null);
+  const [open, setOpen] = React.useState(false);
+  let openedUser = null;
+
+  const handleOpen = (opened) => {
+    openedUser = opened
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
 
   useEffect(() => {
     auth.setUser(user)
@@ -73,7 +89,6 @@ const adminUsers = ({ user }: Props): JSX.Element => {
     </>
     )
   else
-    console.log('ici', users)
     return (
       <>
         <Head>
@@ -81,18 +96,51 @@ const adminUsers = ({ user }: Props): JSX.Element => {
         </Head>
         <Layout backgroundColor={'#161C22'}>
           <Navbar />
-          {users & console.log(users)}
           { users ? users.map((elem, i) => {
               return (
-                <UserCard 
-                  key={i}
-                  name={elem.username}
-                  email={elem.email}
-                  role={elem.role}
-                  id={elem._id} />
+                <Cards style={{ marginBottom: "10px", marginLeft: "25px", marginRight: "25px", cursor: "pointer" }} onClick={handleOpen(elem)}>
+                  <CardContent>
+                    <Typography variant="h5" >
+                      {elem._id}
+                    </Typography>
+                  </CardContent>
+                  <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                      <CardContent>
+                        <Typography  >
+                          {elem.username}
+                        </Typography>
+                      </CardContent>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <CardContent>
+                        <Typography  >
+                          {elem.email}
+                        </Typography>
+                      </CardContent>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <CardContent>
+                        <Typography  >
+                          {elem.role}
+                        </Typography>
+                      </CardContent>
+                    </Grid>
+                  </Grid>
+                </Cards>
               )
-            }) : <p>abc</p>
+            }) : <p>No users found</p>
           }
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <div>
+              <h1>{openedUser}</h1>
+            </div>
+          </Modal>
           <Footer />
         </Layout>
       </>
