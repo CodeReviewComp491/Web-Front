fa8d4bb07ca03472d1a27f15cd1de487c348bd06
alexis-desrumaux <48721665+alexis-desrumaux@users.noreply.github.com>
2021-12-09 21:18:48 +0300

    feat: 🎸 adding community webpage. TopBar design improved (#6)

--- components/global/DashboardLayout/TopBar/TopBar.tsx
+++ components/global/DashboardLayout/TopBar/TopBar.tsx
@@ -1,12 +1,66 @@
-import React from 'react';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Menu, Dropdown, Button } from 'antd'
+
+//store
+import { GlobalState } from 'store/interfaces'
 
 //css
-import * as Styled from 'components/global/DashboardLayout/TopBar/styles';
+import * as Styled from 'components/global/DashboardLayout/TopBar/styles'
 
-const TopBar = (): JSX.Element => {
+interface Props {
+  pageTitle: string
+}
+
+const TopBar = ({ pageTitle }: Props): JSX.Element => {
+  const { user } = useSelector<GlobalState, GlobalState>((state) => state)
+
+  const displayUserDropDown = (): JSX.Element => {
+    return (
+      <Menu>
+        <Menu.Item>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.antgroup.com"
+          >
+            1st menu item
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.aliyun.com"
+          >
+            2nd menu item
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.luohanacademy.com"
+          >
+            3rd menu item
+          </a>
+        </Menu.Item>
+      </Menu>
+    )
+  }
+
   return (
-    <Styled.TopBar/>
+    <Styled.TopBar>
+      <Styled.TopBarCenter>
+        <Styled.PageTitle>{pageTitle}</Styled.PageTitle>
+        <Dropdown overlay={displayUserDropDown()} placement="bottomLeft" arrow>
+          <Styled.Avatar
+            backgroundImage={`https://ui-avatars.com/api/?background=fc032c&color=fff&name=${user.username}`}
+          />
+        </Dropdown>
+      </Styled.TopBarCenter>
+    </Styled.TopBar>
   )
 }
 
-export default TopBar;
+export default TopBar
--- pages/community-reviews.tsx
+++ pages/community-reviews.tsx
@@ -1,17 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import Head from 'next/head'
 
-//store
+//components
+import DashboardLayout from 'components/global/DashboardLayout/DashboardLayout'
+
+//config
+import paths from 'config/routes'
+
+//common
+import { UserState } from 'common/types'
 import { AuthenticationStatus } from 'common/enum'
-import { GlobalState } from 'store/interfaces'
 
-const communityReviews = (): JSX.Element => {
-  /*const { user }: GlobalState = useSelector<GlobalState, GlobalState>(
-    (state) => state,
-  )*/
-  return <></>
+//backend
+import { isUserLogged } from 'backend/utils/tokenChecker'
 
+//hooks
+import useAuth from 'hooks/useAuth'
 
+export async function getServerSideProps(ctx: any) {
+  const user: UserState = await isUserLogged(ctx)
+  if (user.authenticationStatus === AuthenticationStatus.FAILED) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: paths.home.signin.index,
+      },
+    }
+  }
+  return {
+    props: {
+      user: user,
+    },
+  }
 }
 
-export default communityReviews;
+interface Props {
+  user: UserState
+}
+
+const communityReviews = ({ user }: Props): JSX.Element => {
+  const auth = useAuth()
+  const [isMounted, setMounted] = useState<boolean>(false)
+
+  useEffect(() => {
+    auth.setUser(user)
+    setMounted(true)
+  }, [])
+
+  if (isMounted === false) return <></>
+  return (
+    <>
+      <Head>
+        <title>Code Review | {user.username}</title>
+      </Head>
+      <DashboardLayout keySelected={2} pageTitle={'/Community Reviews'}>
+        toto
+      </DashboardLayout>
+    </>
+  )
+}
+
+export default communityReviews
