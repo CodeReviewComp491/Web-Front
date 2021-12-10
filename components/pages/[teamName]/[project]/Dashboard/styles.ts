d95dca93ca781a2fb373b7d4e5e41a8f4e29e95e
Alexis Desrumaux <alexis.desrumaux@epitech.eu>
2021-12-10 07:02:31 +0300

    feat: 🎸 adding-details-review-page

---
+++ components/pages/[teamName]/[project]/Dashboard/styles.ts
@@ -0,0 +1,105 @@
+import styled from 'styled-components'
+import { Button } from 'antd';
+
+export const Dashboard = styled.div`
+  width: 100%;
+  background-color: white;
+  margin-top: 50px;
+`
+
+export const Header = styled.div`
+  width: 100%;
+  padding-left: 15px;
+  padding-right: 15px;
+`
+
+export const ProjectHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`
+
+export const ProjectName = styled.span`
+  font-size: 20px;
+  color: black;
+  margin-right: 10px;
+`
+
+export const ProjectTeamHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`
+
+export const ProjectTeamName = styled.span`
+  font-size: 13px;
+  color: black;
+`;
+
+export const Actions = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
+
+export const ActionsCenter = styled.div`
+  width: 95%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Action = styled.div`
+  width: 49%;
+  border: solid 1px rgba(0, 0, 0, 0.3);
+  padding-left: 5px;
+  padding-right: 5px;
+`;
+
+export const ActionTitle = styled.div`
+  font-size: 20px;
+  color: black;
+`;
+
+export const ActionDescription = styled.div`
+  font-size: 15px;
+  color: black;
+`;
+
+interface MyButtonProps {
+  backgroundColor: string;
+  color: string;
+}
+
+export const MyButton = styled(Button)`
+  margin-top: 20px;
+  margin-bottom: 10px;
+  padding: 20px;
+  padding-left: 10px;
+  padding-right: 10px;
+  background-color: ${(props: MyButtonProps) => props.backgroundColor};
+  border: none;
+  color: ${(props: MyButtonProps) => props.color};
+  font-size: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &:hover, &:focus {
+    background-color: ${(props: MyButtonProps) => props.backgroundColor};
+    color: ${(props: MyButtonProps) => props.color};
+
+  }
+`;
